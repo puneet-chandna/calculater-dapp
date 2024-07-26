06b115fcc592294304dcd340dc49431a5c670357
[Puneet] <puneetchandna21@gmail.com>
2024-07-27 04:52:30 +0530

    Initial commit

--- tests/calculater-dapp.ts
+++ tests/calculater-dapp.ts
@@ -1,12 +1,20 @@
 import * as anchor from '@project-serum/anchor';
-const assert = require('assert');
+import assert from 'assert';
 const { SystemProgram } = anchor.web3;
 
 describe('calculater-dapp', () => {
     const provider = anchor.AnchorProvider.local();
     anchor.setProvider(provider);
+
+    // Explicitly fetch the workspace and program
+    const idl = JSON.parse(
+        require('fs').readFileSync('target/idl/calculater_dapp.json', 'utf8')
+    );
+
+    const programId = new anchor.web3.PublicKey('99PNcvRfaEmDgw4sMBMakmzEazzif7wYUH5kBobJqyjc');
+    const program = new anchor.Program(idl, programId, provider);
+
     const calculater = anchor.web3.Keypair.generate();
-    const program = anchor.workspace.calculater_dapp;
 
     it('creates a calculater', async () => {
         await program.rpc.create("Welcome to Solana", {
@@ -17,6 +25,7 @@ describe('calculater-dapp', () => {
             },
             signers: [calculater]
         });
+
         const account = await program.account.calculater.fetch(calculater.publicKey);
         assert.ok(account.greeting === "Welcome to Solana");
     });
